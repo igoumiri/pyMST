d1b1479baea91ba9c4dfafcb3266604c9d0cfcc6
Imène Goumiri <imene.goumiri@gmail.com>
2017-04-11 22:00:05 -0500

    Remove duplicate files

--- control.py
+++ control.py
@@ -14,13 +14,14 @@ class LQG:
 		self.B = np.mat(config["B"])
 		self.C = np.mat(config["C"])
 		self.D = np.mat(config["D"])
-		# self.F = np.mat(config["F"])
-		self.K = np.mat(config["K"]) * 382
-		self.Ki = np.mat(config["Ki"]) * 1e2
+		self.F = np.mat(config["F"])
+		self.K = np.mat(config["K"])
+		self.Ki = np.mat(config["Ki"])
 		self.L = np.mat(config["L"])
 		self.ud = np.mat(config["ud"])
 		self.xd = np.mat(config["xd"])
-		self.r = np.mat(config["r"])
+		self.r0 = np.mat(config["r0"])
+		self.u0 = np.mat(config["u0"])
 		self.dt = config["dt"]
 
 		self.AmLC = self.A - self.L * self.C
@@ -31,18 +32,22 @@ class LQG:
 		self.xi = np.zeros((q, 1))
 
 	def observe(self, u, y, xh, xi, i):
-		u = np.mat(u).T
-		y = np.mat(y).T
+		u = np.mat(u).T - self.u0
+		y = np.mat(y).T - self.r0
 		self.xh = self.AmLC * self.xh + self.BmLD * u + self.L * y
 		# self.xi += self.dt * (y - self.r)
 		xh[i,:] = self.xh.flat
 		xi[i,:] = self.xi.flat
 
-	def control(self, y, xh, xi, i):
-		y = np.mat(y).T
-		u = self.ud + self.K * (self.xd - self.xh) - self.Ki * self.xi
+	def control(self, r, y, xh, xi, vh, i):
+		y = np.mat(y).T - self.r0
+		u = self.F * (r - self.r0) - self.K * self.xh - self.Ki * self.xi
+
+		vh[i,:] = (u + self.u0).flat
+		u = np.clip(u + self.u0, [[-250], [-40]], [[250], [40]]) - self.u0
+
 		self.xh = self.AmLC * self.xh + self.BmLD * u + self.L * y
-		self.xi += self.dt * (y - self.r)
+		self.xi += self.dt * (y - (r - self.r0))
 		xh[i,:] = self.xh.flat
 		xi[i,:] = self.xi.flat
-		return u.T
+		return (u + self.u0).T
